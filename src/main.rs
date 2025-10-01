54b5f19d6c3327d6044551a073629040cc7e7813
benev0 <benjaminevanownby@gmail.com>
2025-10-01 01:55:56 -0400

    remove unneeded mutation

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,18 @@ use winit::{event_loop::EventLoop};
 
 fn main() -> Result<(), impl Error>{
     let graphics_closed: AtomicBool = AtomicBool::new(false);
-    let mut result = Ok(());
 
     thread::scope(|s| {
         s.spawn(|| {
             let game = crate::world::GameSystems::new();
             while !graphics_closed.load(Relaxed) && game.world().progress() {}
-            println!("closing")
+            println!("graphics closed shutting down engine")
         });
 
         let event_loop = EventLoop::new().unwrap();
         let mut app = crate::graphics::GraphicsDisplay::new(&event_loop);
-        result = event_loop.run_app(&mut app);
+        let result = event_loop.run_app(&mut app);
         graphics_closed.store(true, Relaxed);
-    });
-
-    result
+        result
+    })
 }
