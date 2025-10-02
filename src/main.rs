ed329a767f2f38802fafa73357c50c3076263f41
benev0 <benjaminevanownby@gmail.com>
2025-10-02 00:53:39 -0400

    types valid on service monster

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod graphics;
 mod world;
 
+
 use std::{error::Error, sync::atomic::{AtomicBool, Ordering::Relaxed}};
 use std::thread;
+use std::sync::mpsc::{self, Sender, Receiver};
 
 use winit::{event_loop::EventLoop};
 
@@ -10,10 +12,12 @@ use winit::{event_loop::EventLoop};
 
 fn main() -> Result<(), impl Error>{
     let graphics_closed: AtomicBool = AtomicBool::new(false);
+    let (tx, _rx): (Sender<(f32, f32)>, Receiver<(f32, f32)>) = mpsc::channel();
 
     thread::scope(|s| {
         s.spawn(|| {
             let game = crate::world::GameSystems::new();
+            game.with_render(tx);
             while !graphics_closed.load(Relaxed) && game.world().progress() {}
             println!("graphics closed shutting down engine")
         });
--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,5 @@
 use flecs_ecs::prelude::*;
+use std::{any::{Any, TypeId}, collections::HashMap, sync::{mpsc::Sender, Arc, RwLock}};
 // todo: build basic systems here
 
 #[derive(Component)]
@@ -19,6 +20,7 @@ struct Sprite;
 
 pub struct GameSystems {
     world: World,
+    services: ServiceManager,
     // todo add IOC/services
 }
 
@@ -27,6 +29,7 @@ impl GameSystems {
         let world = World::new();
 
         // init ioc/services
+        let services = ServiceManager::new();
 
         // create systems
         world
@@ -37,31 +40,79 @@ impl GameSystems {
                 println!("{{ {}, {} }}", p.x, p.y);
             });
 
+        // register default entity
         world
-            .system_named::<(&Position, &Sprite)>("render")
-            .each(|(_p, _)| {
-                // accumulate data into buffer
-            });
-
-            world
             .entity()
             .set(Position { x: 10.0, y: 20.0 })
             .set(Velocity { dx: 0.0, dy: 2.0 });
-
-        world
-            .entity()
-            .set(Position { x: 10.0, y: 20.0 })
-            .set(Velocity { dx: 3.0, dy: 4.0 });
-
 
         // set tick rate
         // to do make configurable
         world.set_target_fps(20.0);
 
-        Self { world }
+        Self { world, services }
+    }
+
+    pub fn with_render(&self, tx: Sender<(f32, f32)>){
+        self.world
+            .system_named::<(&Position, &Sprite)>("render")
+            .each(move |(p, _)| {
+                let _ = tx.send((p.x, p.y));
+            });
     }
 
     pub fn world(&self) -> &World {
         &self.world
+    }
+}
+
+
+// services
+pub trait Service: Any + Send + Sync {
+    fn init() -> Self where Self: Sized;
+    fn post_init(&mut self);
+}
+
+struct ServiceManager {
+    services:  RwLock<HashMap<TypeId, Arc<RwLock<Box<dyn Service>>>>>
+}
+
+impl ServiceManager {
+    pub fn new() -> Self {
+        Self { services: RwLock::new(HashMap::new()) }
+    }
+
+    pub fn init_service<T: Service>(&mut self) {
+        let s: T = T::init();
+        let ds: Arc<RwLock<Box<dyn Service>>> = Arc::new( RwLock::new( Box::new(s) ) );
+
+        self.services
+            .write()
+            .expect("service manager poisoned")
+            .insert(TypeId::of::<T>(), ds);
+    }
+
+    pub fn post_init_service<T: Service>(&mut self) {
+        self.get_service::<T>()
+            .map(|service| {
+                let mut binding = service
+                    .write()
+                    .expect("service poisoned");
+
+                binding.post_init();
+            });
+    }
+
+    pub fn get_service<T>(&mut self) -> Option<Arc<RwLock<T>>> where T: Service + Send + Sync {
+        let binding = self.services
+            .write()
+            .expect("service manager poisoned");
+
+        binding
+            .get(&TypeId::of::<T>())
+            .and_then(|service| {
+                let s = service.clone();
+                Arc::downcast::<RwLock<T>>(s).ok()
+            })
     }
 }
